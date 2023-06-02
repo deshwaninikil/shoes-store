041dfe28e1dc3eb2971a7806f432dc69d500b3bd
deshwaninikil <79966144+deshwaninikil@users.noreply.github.com>
2023-06-02 22:54:37 +0530

    Changes in singleproduct missing dependency: 'fetchCurrentProducts' again

--- src/pages/SingleProduct/SingleProduct.jsx
+++ src/pages/SingleProduct/SingleProduct.jsx
@@ -29,18 +29,28 @@ export const SingleProduct = () => {
     (wishlistItem) => wishlistItem._id === currentProduct._id
   );
 
-  const fetchCurrentProducts = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setLoading(false);
-      setSingleProductData(data.product);
-    } catch (error) {
-      console.log("Error in fetching products", error);
-    }
-  };
+  // const fetchCurrentProducts = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const { data } = await axios.get(`/api/products/${productId}`);
+  //     setLoading(false);
+  //     setSingleProductData(data.product);
+  //   } catch (error) {
+  //     console.log("Error in fetching products", error);
+  //   }
+  // };
 
   useEffect(() => {
+    const fetchCurrentProducts = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(`/api/products/${productId}`);
+        setLoading(false);
+        setSingleProductData(data.product);
+      } catch (error) {
+        console.log("Error in fetching products", error);
+      }
+    };
     fetchCurrentProducts();
   }, [productId]);
 
